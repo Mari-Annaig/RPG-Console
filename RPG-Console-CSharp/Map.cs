1558af49f154458b874309c603f010f2ca638506
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-04-26 22:42:46 +0200

    Add Map class

---
+++ RPG-Console-CSharp/Map.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Console_CSharp
+{
+    class Map
+    {
+        public string[,] map;
+
+        // constructor
+        public Map(int w, int h)
+        {
+            map = new string[w,h];
+        }
+
+        public string[,] FillMap(string[,] map)
+        {
+            return map;
+        }
+    }
+}
