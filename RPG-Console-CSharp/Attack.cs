9e799c3e38c0d6b5707e41fbb32533f8b53f7ba1
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-04-26 22:42:36 +0200

    Add Attack class

---
+++ RPG-Console-CSharp/Attack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Console_CSharp
+{
+    class Attack
+    {
+        private string name;
+        private int damage;
+        private int range;
+
+        public string Name { get { return name; } }
+        public int Damage { get { return damage; } }
+        public int Range { get { return range; } }
+
+        public Attack(string name, int damage, int range)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.range = range;
+        }
+
+    }
+}
