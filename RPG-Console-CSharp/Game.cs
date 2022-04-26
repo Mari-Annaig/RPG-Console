87e82839b1d77aef7e97f8f5617910ce2e9307d9
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-04-26 22:42:58 +0200

    Add Game class

---
+++ RPG-Console-CSharp/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Console_CSharp
+{
+    class Game
+    {
+        private Map map;
+        private Perso perso;
+        private List<Enemy> enemiesBase;
+
+        // constructor
+        public Game()
+        {
+            map = new Map(10, 10);
+            perso = new Perso(10, new List<Attack>());
+
+            for (int i = 0; i < 10; i++)
+            {
+                enemiesBase.Add(new Enemy(3, "crachat", 1, 1));
+            }
+        }
+    }
+}
