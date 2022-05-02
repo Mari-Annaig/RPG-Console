ef2ec20edd39e238ce3e01ee03dc47f3506b3344
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-05-02 22:35:27 +0200

    Ajoute les fonctions de remplissement de la carte

--- RPG-Console-CSharp/Map.cs
+++ RPG-Console-CSharp/Map.cs
@@ -1,21 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace RPG_Console_CSharp
 {
     class Map
     {
-        public string[,] map;
+        public char[,] map;
+        private int width;
+        private int height;
 
         // constructor
         public Map(int w, int h)
         {
-            map = new string[w,h];
+            map = new char[w, h];
+            this.width = w;
+            this.height = h;
         }
 
-        public string[,] FillMap(string[,] map)
+        public char[,] FillMap(char[,] map)
         {
+            map = FillBorder(map);
+            return map;
+        }
+
+        private char[,] FillBorder(char[,] map)
+        {
+            Console.WriteLine(map.Length);
+            Console.WriteLine(map.GetLength(0));
+            Console.WriteLine(map.GetLength(1));
+            for (int i = 0; i < this.width; i++)
+            {
+                map[i, 0] = '|';
+                map[i, (this.width - 2)] = '|';
+            }
+            for (int j = 0; j < this.height; j++)
+            {
+                map[0, j] = '_';
+                map[(this.height - 2), j] = '_';
+            }
+            return map;
+        }
+
+        private char[,] FillMapWithFile(string path, char[,] map)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string ln = "";
+                int i = 0;
+                int j = 0;
+                while ((ln = sr.ReadLine()) != null)
+                {
+                    foreach (char c in ln)
+                    {
+                        map[i, j] = c;
+                        ++j;
+                    }
+                    ++i;
+                    j = 0;
+                }
+            }
             return map;
         }
     }
