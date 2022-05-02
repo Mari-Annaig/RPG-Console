5636a2b36276e1264f6433abe34375733939331d
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-05-02 22:37:02 +0200

    Ajoute la classe Perso

--- RPG-Console-CSharp/Perso.cs
+++ RPG-Console-CSharp/Perso.cs
@@ -13,6 +13,64 @@ namespace RPG_Console_CSharp
 
         public Perso(int pv, string attackName, int damage, int range) : base(pv, attackName, damage, range)
         {
+            
+        }
+
+        public void Action(bool inBattle)
+        {
+            Console.WriteLine("Action que vous pouvez faire : ");
+            if (inBattle)
+            {
+                Console.WriteLine("1-Attaquer");
+                Console.WriteLine("2-Esquiver");
+                Console.WriteLine("Entrer le chiffre");
+                
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        // liste des attaques
+                        Console.WriteLine("Vos attaques :");
+                        for (int i = 0; i < this.attacks.Count; i++)
+                        {
+                            Console.WriteLine(i + "-" + this.attacks[i].Name);
+                        }
+                        Console.WriteLine("Entrer le chiffre");
+                        break;
+                    case "2":
+                        // esquive
+                        Console.WriteLine("Esquive");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("1-Passer en mode déplacement");
+                Console.WriteLine("2-Avoir plus d'informations sur l'endroit");
+                Console.WriteLine("3-Ouvrir le menu");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        // passe en mode déplacement
+                        break;
+                    case "2":
+                        // ouvrir le fichier avec plus d'info
+                        break;
+                    case "3":
+                        Console.WriteLine("Quel menu ?\n Merci de choisir une véritable action");
+                        break;
+                    default:
+                        Console.WriteLine("Veuillez entre le chiffre de l'action");
+                        Action(inBattle);
+                        break;
+                }
+            }
+        }
+
+        public void Move()
+        {
 
         }
     }
