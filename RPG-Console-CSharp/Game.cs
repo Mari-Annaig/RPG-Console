9882f79eaf914f1e04244c2ad4b98fc2210fae5b
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-05-02 22:36:45 +0200

    Ajoute le début des fonctions de la classe Game

--- RPG-Console-CSharp/Game.cs
+++ RPG-Console-CSharp/Game.cs
@@ -9,16 +9,57 @@ namespace RPG_Console_CSharp
         private Map map;
         private Perso perso;
         private List<Enemy> enemiesBase;
+        private bool endGame;
 
         // constructor
         public Game()
         {
             map = new Map(10, 10);
             perso = new Perso(10, new List<Attack>());
-
+            enemiesBase = new List<Enemy>();
             for (int i = 0; i < 10; i++)
             {
                 enemiesBase.Add(new Enemy(3, "crachat", 1, 1));
+            }
+            endGame = false;
+        }
+
+        private void PrintMap(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void DescribePlace(string pathFileDescribe)
+        {
+            // lecture du fichier puis affiche le texte
+        }
+
+        public void TheGame(char[,] map)
+        {
+            // Vous avez perdu !
+
+            // TO DO
+            // - déplacements
+            // - combats
+            // - changement de salles/map
+
+            while (!endGame)
+            {
+                // affichage de la map
+                PrintMap(map);
+                // desciption global de l'espace
+                DescribePlace(""); // mettre le path
+
+                // demande une action au joueur
+                
+
             }
         }
     }
