75a1bf09ef515e6ab472aa0cd20069c7dc0f2aea
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-05-03 23:00:06 +0200

    Ajoute le déplacement

--- RPG-Console-CSharp/Game.cs
+++ RPG-Console-CSharp/Game.cs
@@ -10,11 +10,16 @@ namespace RPG_Console_CSharp
         private Perso perso;
         private List<Enemy> enemiesBase;
         private bool endGame;
+        private bool inBattle;
+        private static bool inDeplacement;
+
+        public static bool InDeplacement { get { return inDeplacement; } set { inDeplacement = value; } }
 
         // constructor
         public Game()
         {
             map = new Map(10, 10);
+            map.PieceName = "chambre";
             perso = new Perso(10, new List<Attack>());
             enemiesBase = new List<Enemy>();
             for (int i = 0; i < 10; i++)
@@ -22,6 +27,8 @@ namespace RPG_Console_CSharp
                 enemiesBase.Add(new Enemy(3, "crachat", 1, 1));
             }
             endGame = false;
+            inBattle = false;
+            inDeplacement = false;
         }
 
         private void PrintMap(char[,] map)
@@ -36,12 +43,46 @@ namespace RPG_Console_CSharp
             }
         }
 
-        private void DescribePlace(string pathFileDescribe)
+        private void DescribePlace(bool isInfoEntend, string pieceName)
         {
             // lecture du fichier puis affiche le texte
         }
 
-        public void TheGame(char[,] map)
+        public void UpdatePositionPerso(int x, int y)
+        {
+            // utiliser mapOrigin pour remettre la case dans l'état où elle était
+            if ((map.MyMap.GetLength(0) < x) && (map.MyMap.GetLength(1) < y))
+            {
+                // switch sur les cases à éviter
+                switch (map.MyMap[x,y])
+                {
+                    case 'E':
+                        // enemi donc mode battle
+                        inDeplacement = false;
+                        inBattle = true;
+                        break;
+                    case '|':
+                        // mur, pas aller plus loin
+                        break;
+                    default:
+                        map.MyMap[x, y] = 'P';
+                        switch (map.MapOrigin[x,y])
+                        {
+                            case 'L':
+                                Console.WriteLine("Vous êtes sur un lit");
+                                break;
+                            case 'S':
+                                // escalier
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+
+        public void TheGame()
         {
             // Vous avez perdu !
 
@@ -49,13 +90,27 @@ namespace RPG_Console_CSharp
             // - déplacements
             // - combats
             // - changement de salles/map
-
+            map.FillMapWithFile("../../../Map/" + map.PieceName + ".txt", map.MyMap);
+            PrintMap(map.MyMap);
             while (!endGame)
             {
                 // affichage de la map
-                PrintMap(map);
+                if (inDeplacement)
+                {
+                    int x = 0;
+                    int y = 0;
+
+                    (x, y) = perso.Move();
+                    UpdatePositionPerso(x, y);
+                    Console.Clear();
+                }
+                else
+                {
+                    perso.Action(inBattle);
+                }
+                PrintMap(map.MyMap);
                 // desciption global de l'espace
-                DescribePlace(""); // mettre le path
+                DescribePlace(false, map.PieceName); // mettre le path
 
                 // demande une action au joueur
                 
